e937b9266560c480555715ef2009175275548725
smithgg415 <133823261+smithgg415@users.noreply.github.com>
2024-08-13 16:20:30 -0300

    Add files via upload

---
+++ beachtennis/script/script.js
@@ -0,0 +1,44 @@
+//codigo da tela index.html
+       
+$('#carouselPatrocinadores').carousel({
+    interval: 1300,
+    wrap: true
+});
+
+function showPopup() {
+    document.getElementById('popup-container').classList.add('show');
+}
+
+function closePopup() {
+    document.getElementById('popup-container').classList.remove('show');
+}
+
+window.onload = function () {
+    showPopup();
+};
+//codigo da tela inscricao.html
+        document.getElementById('registrationForm').addEventListener('submit', function (event) {
+            event.preventDefault();
+            $('#confirmationModal').modal('show');
+        });
+
+        document.getElementById('copyPixButton').addEventListener('click', function () {
+            const pixKeyElement = document.getElementById('pix-key');
+            const range = document.createRange();
+            range.selectNode(pixKeyElement);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
+
+            try {
+                const successful = document.execCommand('copy');
+                const msg = successful ? 'successful' : 'unsuccessful';
+                alert('Chave PIX copiada ' + msg);
+            } catch (err) {
+                console.error('Falha ao copiar a chave PIX: ', err);
+            }
+
+            window.getSelection().removeAllRanges();
+        });
+        
+        
+        
