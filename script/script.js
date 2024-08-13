1fef3631afd8c672a3684f03fc72d3910d4c1187
smithgg415 <133823261+smithgg415@users.noreply.github.com>
2024-08-13 14:51:51 -0300

    Commit

--- script/script.js
+++ script/script.js
@@ -1,38 +1,44 @@
 //codigo da tela index.html
+       
+$('#carouselPatrocinadores').carousel({
+    interval: 1300,
+    wrap: true
+});
+
+function showPopup() {
+    document.getElementById('popup-container').classList.add('show');
+}
+
 function closePopup() {
     document.getElementById('popup-container').classList.remove('show');
 }
 
-// Display the popup only once when the main page loads
 window.onload = function () {
-    var hasVisited = sessionStorage.getItem('hasVisited');
-    if (!hasVisited) {
-        document.getElementById('popup-container').classList.add('show');
-        sessionStorage.setItem('hasVisited', 'true');
-    }
+    showPopup();
 };
 //codigo da tela inscricao.html
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    $('#confirmationModal').modal('show');
-});
+        document.getElementById('registrationForm').addEventListener('submit', function (event) {
+            event.preventDefault();
+            $('#confirmationModal').modal('show');
+        });
 
-document.getElementById('copyPixButton').addEventListener('click', function () {
-    const pixKeyElement = document.getElementById('pix-key');
-    const range = document.createRange();
-    range.selectNode(pixKeyElement);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
+        document.getElementById('copyPixButton').addEventListener('click', function () {
+            const pixKeyElement = document.getElementById('pix-key');
+            const range = document.createRange();
+            range.selectNode(pixKeyElement);
+            window.getSelection().removeAllRanges();
+            window.getSelection().addRange(range);
 
-    try {
-        const successful = document.execCommand('copy');
-        const msg = successful ? 'successful' : 'unsuccessful';
-        alert('Chave PIX copiada ' + msg);
-    } catch (err) {
-        console.error('Falha ao copiar a chave PIX: ', err);
-    }
+            try {
+                const successful = document.execCommand('copy');
+                const msg = successful ? 'successful' : 'unsuccessful';
+                alert('Chave PIX copiada ' + msg);
+            } catch (err) {
+                console.error('Falha ao copiar a chave PIX: ', err);
+            }
 
-    window.getSelection().removeAllRanges();
-});
-
-
+            window.getSelection().removeAllRanges();
+        });
+        
+        
+        
